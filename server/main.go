1a16961f2ae0abdcf0c2a0f5b175cf5e7d199ad5
Ayush Maheshwari <ayush.maheshwari@sjsu.edu>
2021-09-20 13:59:10 -0700

    even more cleanup

--- server/main.go
+++ server/main.go
@@ -11,15 +11,15 @@ import (
 	"syscall"
 )
 
-type HandlersWrapper struct {
+type App struct {
 	db            interfaces.MongoInterface
 	router        interfaces.Router
 	hostIpBinding string
 	frontEndPath  string
 }
 
-func NewApp() *HandlersWrapper {
-	app := new(HandlersWrapper)
+func NewApp() *App {
+	app := new(App)
 
 	app.router = router.NewGorillaRouter()
 	app.hostIpBinding = os.Getenv("HOST_IP_BINDING")
@@ -32,19 +32,19 @@ func NewApp() *HandlersWrapper {
 	return app
 }
 
-func (h *HandlersWrapper) Start() {
-	h.db.Connect()
+func (a *App) Start() {
+	a.db.Connect()
 
-	fileServer := http.FileServer(http.Dir(h.frontEndPath))
-	h.router.GetRouter().Path("/").Handler(fileServer)
-	handlers.NewHandlers(h.router)
+	fileServer := http.FileServer(http.Dir(a.frontEndPath))
+	a.router.GetRouter().Path("/").Handler(fileServer)
+	handlers.NewHandlers(a.router)
 
-	h.router.Serve(h.hostIpBinding)
+	a.router.Serve(a.hostIpBinding)
 }
 
-func (h *HandlersWrapper) Stop() {
-	h.router.Stop()
-	h.db.Stop()
+func (a *App) Stop() {
+	a.router.Stop()
+	a.db.Stop()
 }
 
 func main() {
