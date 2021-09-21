331135cd31cce52cabc27e992d4ab9129490e5e3
Ayush Maheshwari <ayush.maheshwari@sjsu.edu>
2021-09-20 19:39:51 -0700

    rest api example

--- server/handlers/requestHandlers.go
+++ server/handlers/requestHandlers.go
@@ -12,8 +12,17 @@ func (h *Handlers) SaveNewUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Handlers) LoginRequest(w http.ResponseWriter, r *http.Request) {
+	var courses []*middleware.Course
+
 	course := new(middleware.Course)
 	course.Name = "CS160"
 	course.Department = "computer science"
-	fmt.Fprint(w, utils.Jsonify(course))
+	courses = append(courses, course)
+
+	course2 := new(middleware.Course)
+	course2.Name = "PHIL134"
+	course2.Department = "philosophy"
+	courses = append(courses, course2)
+
+	fmt.Fprint(w, utils.Jsonify(courses))
 }
