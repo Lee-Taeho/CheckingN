5bb04b9d1592aab720f43041e81c377e5c0b2ef9
kharanga <60679652+kharanga@users.noreply.github.com>
2021-09-23 03:22:58 -0700

    Update requestHandlers.go

--- server/handlers/requestHandlers.go
+++ server/handlers/requestHandlers.go
@@ -6,7 +6,17 @@ import (
 	"net/http"
 	"server/middleware"
 	"server/utils"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
 )
+
+var jwtKey = []byte("secret_key")
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
 
 func (h *Handlers) ExampleJsonReponse(w http.ResponseWriter, r *http.Request) {
 	var courses []*middleware.Course
@@ -48,7 +58,7 @@ func (h *Handlers) LoginRequest(w http.ResponseWriter, r *http.Request) {
 	log.Println("INFO [handlers/requestHandlers.go] Request to Log In")
 	r.ParseForm()
 	login := &middleware.LoginRequest{
-		Email: r.PostForm.Get("Email"),
+		Email:    r.PostForm.Get("Email"),
 		Password: r.PostForm.Get("Password"),
 	}
 
@@ -58,10 +68,73 @@ func (h *Handlers) LoginRequest(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	expirationTime := time.Now().Add(time.Second * 20)
+
+	claims := &Claims{
+		Username: login.Email,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	http.SetCookie(w,
+		&http.Cookie{
+			Name:    "token",
+			Value:   tokenString,
+			Expires: expirationTime,
+		})
+
 	log.Println("INFO [handlers/requestHandlers.go] Successful Log In")
 	http.Redirect(w, r, "/api/login_request_success", http.StatusSeeOther)
 }
 
 func (h *Handlers) LoginRequestSuccess(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Successful Login!<h1>")
+	if h.TokenValid(w, r) {
+		fmt.Fprint(w, "<h1>Successful Login!<h1>")
+	}
+}
+
+func (h *Handlers) TokenValid(w http.ResponseWriter, r *http.Request) bool {
+	cookie, err := r.Cookie("token")
+
+	if err != nil {
+		if err == http.ErrNoCookie {
+			w.WriteHeader(http.StatusUnauthorized)
+			return false
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return false
+	}
+
+	tokenStr := cookie.Value
+
+	claims := &Claims{}
+
+	tkn, err := jwt.ParseWithClaims(tokenStr, claims,
+		func(t *jwt.Token) (interface{}, error) {
+			return jwtKey, nil
+		})
+
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			w.WriteHeader(http.StatusUnauthorized)
+			return false
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return false
+	}
+
+	if !tkn.Valid {
+		w.WriteHeader(http.StatusUnauthorized)
+		return false
+	}
+	return true
 }
