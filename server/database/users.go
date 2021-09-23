e5c65f5561d3c109ff3b5c08a0c98ca7e75231d3
kharanga <60679652+kharanga@users.noreply.github.com>
2021-09-22 18:02:35 -0700

    Add files via upload

--- server/database/users.go
+++ server/database/users.go
@@ -2,20 +2,44 @@ package database
 
 import (
 	"context"
+	"errors"
+	"log"
 	"server/middleware"
+
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func (m *MongoDB) CreateNewStudent(student middleware.Student) error {
 	ctx := context.TODO()
-	collection := m.mongo.Database(USER_DATABASE).Collection(STUDENTS_COLLECTION)
-	_, err := collection.InsertOne(ctx, student)
-	return err
+	check := m.CheckForValidEmail(student)
+	if check {
+		collection := m.mongo.Database(USER_DATABASE).Collection(STUDENTS_COLLECTION)
+		_, err := collection.InsertOne(ctx, student)
+		return err
+	}
+	return errors.New("email already exist")
 }
 
-func (m *MongoDB) FindStudent(login middleware.LoginRequest) bool {
+func (m *MongoDB) CheckForValidEmail(student1 middleware.Student) bool {
 	ctx := context.TODO()
 	collection := m.mongo.Database(USER_DATABASE).Collection(STUDENTS_COLLECTION)
-	result := collection.FindOne(ctx, login)
-	if result.Err() != nil {return false}
+	cursor, err := collection.Find(ctx, bson.M{})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var students []bson.M
+
+	if err = cursor.All(ctx, &students); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, student2 := range students {
+		if student2["email"] == student1.Email {
+			return false
+		}
+	}
+
 	return true
 }
