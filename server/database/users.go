ac11ef8e85cc56dbaa38d30c64342874f02d72ac
Ayush Maheshwari <ayush.maheshwari@sjsu.edu>
2021-09-22 18:23:07 -0700

    hot fix for duplicate user

--- server/database/users.go
+++ server/database/users.go
@@ -2,12 +2,16 @@ package database
 
 import (
 	"context"
+	"errors"
 	"server/middleware"
 )
 
 func (m *MongoDB) CreateNewStudent(student middleware.Student) error {
 	ctx := context.TODO()
 	collection := m.mongo.Database(USER_DATABASE).Collection(STUDENTS_COLLECTION)
+	if m.FindStudent(middleware.LoginRequest{Email: student.Email, Password: student.Password}) {
+		return errors.New("Student already exists")
+	}
 	_, err := collection.InsertOne(ctx, student)
 	return err
 }
@@ -16,6 +20,8 @@ func (m *MongoDB) FindStudent(login middleware.LoginRequest) bool {
 	ctx := context.TODO()
 	collection := m.mongo.Database(USER_DATABASE).Collection(STUDENTS_COLLECTION)
 	result := collection.FindOne(ctx, login)
-	if result.Err() != nil {return false}
+	if result.Err() != nil {
+		return false
+	}
 	return true
 }
