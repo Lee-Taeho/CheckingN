d8679aefdff77c5e4b0c0c4ed521c83188895c19
kharanga <60679652+kharanga@users.noreply.github.com>
2021-09-24 07:23:53 -0700

    Update requestHelpers.go

--- server/handlers/requestHelpers.go
+++ server/handlers/requestHelpers.go
@@ -1,1 +1,44 @@
 package handlers
+
+import (
+	"net/http"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+func (h *Handlers) tokenValid(w http.ResponseWriter, r *http.Request) bool {
+	cookie, err := r.Cookie("token")
+
+	if err != nil {
+		if err == http.ErrNoCookie {
+			w.WriteHeader(http.StatusUnauthorized)
+			return false
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return false
+	}
+
+	tokenStr := cookie.Value
+
+	claims := &Claims{}
+
+	tkn, err := jwt.ParseWithClaims(tokenStr, claims,
+		func(t *jwt.Token) (interface{}, error) {
+			return jwtKey, nil
+		})
+
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			w.WriteHeader(http.StatusUnauthorized)
+			return false
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return false
+	}
+
+	if !tkn.Valid {
+		w.WriteHeader(http.StatusUnauthorized)
+		return false
+	}
+	return true
+}
