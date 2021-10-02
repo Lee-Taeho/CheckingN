a6e553ce7eb522bc4b43a254a426ca024feb2622
kharanga <60679652+kharanga@users.noreply.github.com>
2021-10-02 01:19:48 -0700

    Update requestHelpers.go

--- server/handlers/requestHelpers.go
+++ server/handlers/requestHelpers.go
@@ -2,9 +2,22 @@ package handlers
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/dgrijalva/jwt-go"
 )
+
+const (
+	AUTO_LOGOUT_TIME    = time.Second * 20
+	TIME_BEFORE_EXPIRED = time.Second * 30
+)
+
+var jwtKey = []byte("secret_key")
+
+type Claims struct {
+	Email string `json:"email"`
+	jwt.StandardClaims
+}
 
 func (h *Handlers) tokenValid(w http.ResponseWriter, r *http.Request) bool {
 	cookie, err := r.Cookie("token")
@@ -41,4 +54,30 @@ func (h *Handlers) tokenValid(w http.ResponseWriter, r *http.Request) bool {
 		return false
 	}
 	return true
+}
+
+func (h *Handlers) createToken(w http.ResponseWriter, r *http.Request, email string) {
+	expirationTime := time.Now().Add(AUTO_LOGOUT_TIME)
+
+	claims := &Claims{
+		Email: email,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	http.SetCookie(w,
+		&http.Cookie{
+			Name:    "token",
+			Value:   tokenString,
+			Expires: expirationTime,
+		})
 }
