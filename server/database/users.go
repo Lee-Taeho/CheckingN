009b0589620c9f660f30c5618c9fd9800047104e
Ayush Maheshwari <ayush.maheshwari@sjsu.edu>
2021-10-25 21:48:11 -0700

    google login is treated like a regular user

--- server/database/users.go
+++ server/database/users.go
@@ -2,20 +2,18 @@ package database
 
 import (
 	"context"
-	"errors"
 	"server/middleware"
 
 	"go.mongodb.org/mongo-driver/bson"
 )
 
-func (m *MongoDB) CreateNewStudent(student middleware.Student) error {
+func (m *MongoDB) CreateNewStudent(student middleware.Student) {
 	ctx := context.TODO()
 	collection := m.mongo.Database(USER_DATABASE).Collection(STUDENTS_COLLECTION)
-	if m.FindStudent(middleware.LoginRequest{Email: student.Email, Password: student.Password}) != nil {
-		return errors.New("Student already exists")
+	result := collection.FindOne(ctx, bson.M{"email": student.Email})
+	if result.Err() != nil {
+		collection.InsertOne(ctx, student)
 	}
-	_, err := collection.InsertOne(ctx, student)
-	return err
 }
 
 func (m *MongoDB) CreateNewGoogleStudent(student middleware.GoogleUser) {
--- server/handlers/loginHandlers.go
+++ server/handlers/loginHandlers.go
@@ -21,11 +21,7 @@ func (h *Handlers) SaveNewUser(w http.ResponseWriter, r *http.Request) {
 	uuid := h.db.GetUUID()
 	student.Uuid = uuid
 
-	if err := h.db.CreateNewStudent(*student); err != nil {
-		log.Printf("%s Couldn't Save New User: %s\n", LOGGER_ERROR_LOGIN, err.Error())
-		w.WriteHeader(http.StatusSeeOther)
-	}
-
+	h.db.CreateNewStudent(*student)
 	log.Println(LOGGER_INFO_LOGIN + " Successfully Saved New User")
 }
 
@@ -52,7 +48,6 @@ func (h *Handlers) LoginRequest(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Write([]byte(utils.Jsonify(header)))
-	// r.Header.Add("Authorization", "Bearer "+encrypt(aes_key, fmt.Sprint(student.Uuid)))
 	log.Println(LOGGER_INFO_LOGIN + " Log In Successful")
 }
 
@@ -66,62 +61,3 @@ func (h *Handlers) Authorized(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusUnauthorized)
 	}
 }
-
-// func (h *Handlers) Refresh(w http.ResponseWriter, r *http.Request) {
-// 	cookie, err := r.Cookie("token")
-// 	if err != nil {
-// 		if err == http.ErrNoCookie {
-// 			w.WriteHeader(http.StatusUnauthorized)
-// 			return
-// 		}
-// 		w.WriteHeader(http.StatusBadRequest)
-// 		return
-// 	}
-
-// 	tokenStr := cookie.Value
-
-// 	claims := &Claims{}
-
-// 	tkn, err := jwt.ParseWithClaims(tokenStr, claims,
-// 		func(t *jwt.Token) (interface{}, error) {
-// 			return jwtKey, nil
-// 		})
-
-// 	if err != nil {
-// 		if err == jwt.ErrSignatureInvalid {
-// 			w.WriteHeader(http.StatusUnauthorized)
-// 			return
-// 		}
-// 		w.WriteHeader(http.StatusBadRequest)
-// 		return
-// 	}
-// 	if !tkn.Valid {
-// 		w.WriteHeader(http.StatusUnauthorized)
-// 		return
-// 	}
-
-// 	if time.Unix(claims.ExpiresAt, 0).Sub(time.Now()) > TIME_BEFORE_EXPIRED {
-// 		w.WriteHeader(http.StatusBadRequest)
-// 		return
-// 	}
-
-// 	expirationTime := time.Now().Add(AUTO_LOGOUT_TIME)
-
-// 	claims.ExpiresAt = expirationTime.Unix()
-
-// 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-// 	tokenString, err := token.SignedString(jwtKey)
-
-// 	if err != nil {
-// 		w.WriteHeader(http.StatusInternalServerError)
-// 		return
-// 	}
-
-// 	http.SetCookie(w,
-// 		&http.Cookie{
-// 			Name:    "token",
-// 			Value:   tokenString,
-// 			Expires: expirationTime,
-// 		})
-
-// }
