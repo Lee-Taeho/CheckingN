457b52e6090321fca9d7f9b38769466730457986
NhienLam <nhienlam.ricky@gmail.com>
2021-11-11 10:28:05 -0800

    added style to confirmation page

--- client/src/pages/AppointmentConfirmation.jsx
+++ client/src/pages/AppointmentConfirmation.jsx
@@ -9,27 +9,42 @@ const AppointmentConfirmation = () => {
     const { tutor } = useParams();
     const { location } = useParams();
 
-    let history = useHistory();
+    const handleConfirm = async (e) => {
+        console.log('confirm')
+        // e.preventDefault();
+
+        // var request = {
+        //     method: 'POST',
+        //     headers: {'Content-Type': 'application/json'},
+        //     body: JSON.stringify({
+        //         student: ,
+        //         tutor: ,
+        //         course: ,
+        //         date: ,
+        //         time: ,
+        //         location: ,
+        //     })
+        // }
+    }
 
     // uri: /tutoring/departments/:course/:date/:time/:tutor/:location/confirmation
     // eg. http://localhost:3000/tutoring/departments/CS174/12-23-2021/12:22:00/Nhien%20Lam/online/confirmation
     return (
-        <div>
-            <h4>New Appointment</h4>
+        <div className='confirmation-container'>
+            <h4 className="title">New Appointment</h4>
             <div>
-                <p>Tutor: {tutor}</p>
-                <p>Course: {course}</p>
-                <p>Date: {date}</p>
-                <p>Time: {time}</p>
-                <p>Location: {location}</p>
+                <p><b>Tutor:</b> {tutor}</p>
+                <p><b>Course:</b> {course}</p>
+                <p><b>Date:</b> {date}</p>
+                <p><b>Time:</b> {time}</p>
+                <p><b>Location:</b> {location}</p>
             </div>
-            <button>Confirm</button>
-            <button onClick={() => {
-                history.push("/tutoring/departments/" + course + "/" + date + "/" + time + "/" + tutor + "/" + location);
-            }}>Cancel</button>
-            <Link to={"/tutoring/departments/" + course}>
-                <button>Cancel</button>
-            </Link>
+            <div className='btn-container'>
+                <button className='btn-primary' onClick={handleConfirm}>Confirm</button>
+                <Link to={"/tutoring/departments/" + course}>
+                    <button className='btn-primary'>Cancel</button>
+                </Link>
+            </div>
         </div>
     );
 };
--- server/run.sh
+++ server/run.sh
@@ -1,5 +1,5 @@
 export HOST_IP_BINDING=localhost:8080
 export FRONT_END_PATH=../client
-export DB_URI="mongodb+srv://nhien:hello123@checkingn.sqysj.mongodb.net/users?retryWrites=true&w=majority"
+export DB_URI=
 
 ./server
